59dc137c25f130dadeb9e3cfbaa8e49e4dc82612
Chee Quee <mitchellcq@gmail.com>
2020-10-13 18:13:49 +1100

    added requirements for external packages, added some pseudo code for inquirer form

---
+++ index.js
@@ -0,0 +1,17 @@
+// External packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const axios = require('axios');
+
+// questions with inquirer
+    //project name --> title of readme
+    //repo name
+    //github username
+    //project description
+    //installation instructions
+    //usage info
+    //how to contribute
+    //test instructions
+    //email
+    //each in respective area
