d658ae6529a9b588836edcb45d25b32af0e8601b
Chee Quee <mitchellcq@gmail.com>
2020-10-19 12:30:36 +1100

    added generate readme generator function, having issue with axios not being found in internal module

--- index.js
+++ index.js
@@ -7,18 +7,6 @@ const util = require('util');
 const api = require('./modules/api.js');
 const generateReadme = require('./modules/generateReadme.js');
 
-//questions with inquirer:
-//project name --> title of readme
-//repo name
-//github username
-//project description
-//installation instructions
-//usage info
-//how to contribute
-//test instructions
-//email
-//each in respective area
-
 const questions = [
     {
         type: 'input',
@@ -60,12 +48,44 @@ const questions = [
         }
     },
     {
+        type: 'input',
+        message: "What do you need to do to install this application?",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "What do you need to do to make this application work?",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
         type: 'list',
         message: "Choose a license for your project.",
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
         name: 'license'
     }
 ];
+
+function writeNewFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success! Your README.md file has been generated")
+    });
+}
+
+const writeFileAsync = util.promisify(writeNewFile);
 
 //async function awaiting all info inputs and api calls
 async function init() {
@@ -77,5 +97,19 @@ async function init() {
 
         //Github api call
         const github = await api.getuser(answers);
+        console.log("You're Github account: ", github);
+
+        //send answers and github to readme generator
+        console.log("Please wait while we generate your Readme.")
+        const readme = generateReadme(answers, github);
+        console.log(readme);
+
+        // write to file
+        await writeFileAsync('NewReadme.md', readme);
+
+    } catch (error) {
+        console.log('error');
     }
 };
+
+init();
--- modules/generateReadme.js
+++ modules/generateReadme.js
@@ -1,1 +1,138 @@
+function generateReadme(answers, github) {
+
+    // Generate Table of Contents conditionally based on userResponses
+    let draftToC = `## Table of Contents`;
+
+    if (answers.installation !== '') {
+        draftToC += `
+    * [Installation](#installation)` };
+
+    if (answers.usage !== '') {
+        draftToC += `
+    * [Usage](#usage)` };
+
+    if (answers.contributing !== '') {
+        draftToC += `
+    * [Contributing](#contributing)` };
+
+    if (answers.tests !== '') {
+        draftToC += `
+    * [Tests](#tests)` };
+
+
+    // Generate markdown for the top required portions of the README
+    let draftMarkdown =
+        `# ${answers.title}
+    ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+    
+    Check out the badges hosted by [shields.io](https://shields.io/).
+    
+    
+    ## Description: 
+    
+    ${answers.description}
+    `
+
+    // Add Table of Contents to markdown
+    draftMarkdown += draftToC;
+
+    // Add License section since License is required to Table of Contents
+    draftMarkdown += `
+    * [License](#license)`;
+
+
+    // Optional Installation section
+    if (answers.installation !== '') {
+
+        draftMarkdown +=
+            `
+    
+    ## Installation
+    
+    *Steps required to install project and how to get the development environment running:*
+    
+    ${answers.installation}`
+    };
+
+
+    // Optional Usage section
+    if (answers.usage !== '') {
+
+        draftMarkdown +=
+
+            `
+    
+    ## Usage 
+    
+    *Instructions and examples for use:*
+    
+    ${answers.usage}`
+    };
+
+
+    // Optional Contributing section
+    if (answers.contributing !== '') {
+        `
+    
+    ## Contributing
+    
+    *Contributing is easy, just follow these instructions!*
+    
+    ${answers.contributing}`
+    };
+
+
+    // Optional Tests section
+    if (answers.tests !== '') {
+
+        draftMarkdown +=
+            `
+    
+    ## Tests
+    
+    *Tests for application and how to run them:*
+    
+    ${answers.tests}`
+    };
+
+
+    // License section is required
+    draftMarkdown +=
+        `
+    
+    ## License
+    
+    ${answers.license}
+    `;
+
+
+    // Questions / About Developer section
+    let draftDev =
+        `
+    ---
+    
+    ## Questions?
+    
+    ![Developer Profile Picture](${github.avatar_url}) 
+    
+    Please contact me with any questions or comments:
+   
+    GitHub: [@${github.login}](${github.url})
+    `;
+
+    // If GitHub email is not null, add to Developer section
+    if (github.email !== null) {
+
+        draftDev +=
+            `
+    Email: ${github.email}
+    `};
+
+    // Add developer section to markdown
+    draftMarkdown += draftDev;
+
+    // Return markdown
+    return draftMarkdown;
+
+}
 module.exports = generateReadme;
